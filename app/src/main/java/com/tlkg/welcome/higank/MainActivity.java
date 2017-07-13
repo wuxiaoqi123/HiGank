e9e443b52db150e19a22a611dd458ed013af1969
wuxiaoqi <wu.xiaoqi@tlkg.com>
2017-07-13 14:39:55 +0800

    修改侧滑菜单

--- app/src/main/java/com/tlkg/welcome/higank/MainActivity.java
+++ app/src/main/java/com/tlkg/welcome/higank/MainActivity.java
@@ -1,7 +1,10 @@
 package com.tlkg.welcome.higank;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -9,6 +12,7 @@ import android.widget.Toast;
 
 import com.tlkg.welcome.higank.base.BaseActivity;
 import com.tlkg.welcome.higank.statusbar.StatusBarUtil;
+import com.umeng.analytics.MobclickAgent;
 
 import butterknife.InjectView;
 
@@ -19,6 +23,9 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     @InjectView(R.id.status_bar_view)
     View statusBarView;
+
+    @InjectView(R.id.nav_view)
+    NavigationView navView;
 
     @InjectView(R.id.ll_title_menu)
     FrameLayout titleMenu;
@@ -34,6 +41,25 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         ViewGroup.LayoutParams layoutParams = statusBarView.getLayoutParams();
         layoutParams.height = StatusBarUtil.getStatusBarHeight(MainActivity.this);
         statusBarView.requestLayout();
+
+        initLeftMenu();
+    }
+
+    private void initLeftMenu() {
+        navView.setItemIconTintList(null);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.exit:
+                        MobclickAgent.onKillProcess(MainActivity.this.getApplicationContext());
+                        finish();
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     @Override
