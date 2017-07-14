62c6f33e2f92f4eb949fc70c5494532936438017
wuxiaoqi <wu.xiaoqi@tlkg.com>
2017-07-14 11:51:11 +0800

    修改底部导航栏按钮

--- app/src/main/java/com/tlkg/welcome/higank/MainActivity.java
+++ app/src/main/java/com/tlkg/welcome/higank/MainActivity.java
@@ -3,6 +3,7 @@ package com.tlkg.welcome.higank;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,11 +11,13 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.roughike.bottombar.BottomBar;
 import com.tlkg.welcome.higank.base.BaseActivity;
 import com.tlkg.welcome.higank.statusbar.StatusBarUtil;
 import com.umeng.analytics.MobclickAgent;
 
 import butterknife.InjectView;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
@@ -24,11 +27,17 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @InjectView(R.id.status_bar_view)
     View statusBarView;
 
+    @InjectView(R.id.tool_bar_head_iv)
+    CircleImageView toolBarHeadIv;
+
     @InjectView(R.id.nav_view)
     NavigationView navView;
 
     @InjectView(R.id.ll_title_menu)
     FrameLayout titleMenu;
+
+    @InjectView(R.id.bottomBar)
+    BottomBar bottomBar;
 
     @Override
     protected int getContentView() {
@@ -41,7 +50,28 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         ViewGroup.LayoutParams layoutParams = statusBarView.getLayoutParams();
         layoutParams.height = StatusBarUtil.getStatusBarHeight(MainActivity.this);
         statusBarView.requestLayout();
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                toolBarHeadIv.clearAnimation();
+                toolBarHeadIv.animate().alpha(1 - slideOffset).setDuration(0).start();
+            }
 
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
         initLeftMenu();
     }
 
@@ -50,6 +80,17 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                onClickLeftMenu(item);
+                return true;
+            }
+        });
+    }
+
+    private void onClickLeftMenu(@NonNull final MenuItem item) {
+        drawerLayout.closeDrawer(Gravity.START);
+        drawerLayout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
                 switch (item.getItemId()) {
                     case R.id.exit:
                         MobclickAgent.onKillProcess(MainActivity.this.getApplicationContext());
@@ -57,9 +98,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                         android.os.Process.killProcess(android.os.Process.myPid());
                         break;
                 }
-                return true;
             }
-        });
+        }, 290);
     }
 
     @Override
